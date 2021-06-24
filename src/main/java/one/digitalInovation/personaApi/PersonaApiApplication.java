18991f5bd99564fa2e1af293fc831fce0fad5405
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-06-24 02:04:41 -0300

    First Commit

---
+++ src/main/java/one/digitalInovation/personaApi/PersonaApiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalInovation.personaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaApiApplication.class, args);
+	}
+
+}
